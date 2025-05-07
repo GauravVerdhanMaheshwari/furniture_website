cb7ea60c24c3c0acbb7aaa90c045913143bcbe84
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-07 16:30:27 +0530

    add animation on shop now

--- Furniture_Frontend/src/components/Home/Hero/Hero.jsx
+++ Furniture_Frontend/src/components/Home/Hero/Hero.jsx
@@ -6,7 +6,7 @@ function Hero() {
       <div className="bg-gray-200 flex-1/5 flex flex-col justify-center items-end p-20">
         <h1 className="text-4xl">Welcome to our shop</h1>
         <p className="text-lg mt-4">Find the best furniture for you</p>
-        <button className="mt-6 px-5 py-2.5 rounded bg-red-200">
+        <button className="mt-6 px-5 py-2.5 rounded-xl bg-red-200 hover:bg-red-300 hover:scale-105 transition-all duration-300 text-black font-bold text-xl active:scale-95 active:bg-red-400">
           Shop Now !
         </button>
       </div>
