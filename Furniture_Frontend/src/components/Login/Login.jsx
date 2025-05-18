cf038a07bef702509405eef5012cdd5aae9092d5
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-18 22:49:40 +0530

    implement login functionality with form validation and password visibility toggle

--- Furniture_Frontend/src/components/Login/Login.jsx
+++ Furniture_Frontend/src/components/Login/Login.jsx
@@ -1,8 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function Login() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleLogin = async () => {
+    const password = document.getElementById("password").value;
+    const username = document.getElementById("username").value;
+    const email = document.getElementById("email").value;
+    if (!password && !username && !email) {
+      alert("Please fill in all fields");
+      return;
+    }
+    const data = {
+      username: username,
+      email: email,
+      password: password,
+    };
+
+    try {
+      const response = await fetch("http://localhost:3000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const result = await response.json();
+      console.log("Server response:", result);
+    } catch (error) {
+      console.error("Failed to send login data to server:", error);
+    }
+  };
+
   return (
-    <div className="mt-25 bg-gray-100 min-h-[70vh] flex items-center justify-center">
+    <div className="mt-20 py-7 bg-gray-100 min-h-[70vh] flex items-center justify-center">
       <form className="flex flex-col items-center justify-center w-full max-w-sm mx-auto bg-white shadow-md rounded px-15 py-10 mb-4">
         <h2 className="text-2xl font-bold mb-6">Login</h2>
         <div className="mb-4">
@@ -46,26 +79,42 @@ function Login() {
           </label>
           <div className="flex flex-row items-center">
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               id="password"
               name="password"
               required
+              min={8}
+              max={20}
+              autoComplete="on"
               className="mt-1 inline w-80 border border-gray-300 rounded-md shadow-sm py-2 px-2 text-start"
               placeholder="Enter your password"
             />
             <img
-              src="hide.webp"
+              src={showPassword ? "hide.webp" : "view.webp"}
               alt=""
               className=" cursor-pointer inline w-7 h-7 mt-1"
+              onClick={() => {
+                setShowPassword(!showPassword);
+              }}
             />
           </div>
         </div>
         <button
           type="submit"
-          className="bg-blue-500 text-white font-bold py-2 px-8 rounded hover:bg-blue-700 transition duration-300 ease-in-out cursor-pointer"
+          className="bg-blue-500 text-white font-bold py-2 my-1 px-8 rounded hover:bg-blue-700 transition duration-300 ease-in-out cursor-pointer"
+          onClick={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
         >
           Login
         </button>
+        <NavLink
+          to="/register"
+          className="text-blue-500 hover:underline mt-2 transition duration-300 ease-in-out"
+        >
+          Don't have an account? Register here
+        </NavLink>
       </form>
     </div>
   );
