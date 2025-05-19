652f3df1f5fd10a29d37e049cdeb8ca341457718
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-19 20:16:47 +0530

    add AdminFooter component for admin dashboard

---
+++ Furniture_Frontend/src/components/AdminFooter/AdminFooter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AdminFooter() {
+  return (
+    <div className="bg-gray-800 text-white p-4">
+      <div className="container mx-auto text-center">
+        <p>© 2025 Geetanjali Furniture. All rights reserved.</p>
+      </div>
+    </div>
+  );
+}
+
+export default AdminFooter;
