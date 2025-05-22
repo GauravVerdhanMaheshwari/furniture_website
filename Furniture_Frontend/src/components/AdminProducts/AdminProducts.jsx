890102f722d128763d8a040a2965054036e97a05
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-22 22:31:52 +0530

    style: update layout and add search functionality in AdminProducts component

--- Furniture_Frontend/src/components/AdminProducts/AdminProducts.jsx
+++ Furniture_Frontend/src/components/AdminProducts/AdminProducts.jsx
@@ -1,26 +1,40 @@
 import React from "react";
+import Products from "./Products";
 
 function AdminProducts() {
   const [products, setProducts] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
   fetch("http://localhost:3000/api/owner/furniture")
     .then((response) => response.json())
     .then((data) => {
       setProducts(data);
+      setLoading(false);
     })
     .catch((error) => {
       console.error("Error fetching data:", error);
     });
   return (
-    <div className="flex flex-col mt-20 items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold">Admin Products</h1>
-      <div className="mt-4">
-        {products.map((product) => (
-          <div key={product.id} className="border p-4 mb-4">
-            <h2 className="text-xl font-semibold">{product.name}</h2>
-            <p className="text-gray-600">{product.description}</p>
-            <p className="font-bold">${product.price}</p>
+    <div className="flex flex-col mt-25 items-center justify-center min-h-screen bg-gray-100">
+      <div className="flex flex-col items-center justify-center bg-gray-100">
+        <div className="flex flex-row items-center justify-between w-full p-4 bg-white shadow-md rounded-xl">
+          <div className="flex items-center m-4">
+            <h1>Filter by product</h1>
           </div>
-        ))}
+          <div>
+            <input
+              type="text"
+              placeholder="Search by name"
+              className="border border-gray-300 rounded p-2"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center bg-gray-100 mt-4">
+        {loading ? (
+          <p className="text-gray-500 text-3xl">Loading...</p>
+        ) : (
+          <Products products={products} />
+        )}
       </div>
     </div>
   );
