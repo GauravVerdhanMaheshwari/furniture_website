2abeb802f0a16467bc02829dac65a87e601f5a51
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-30 19:07:11 +0530

    feat: add user model with schema definition for name, email, password, address, and phone

---
+++ Furniture_Backend/Models/user.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 100,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    address: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 1,
+      maxlength: 500,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      minlength: 10,
+      maxlength: 10,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
