90414d638639720e9317cd9282085090863bf4fe
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-30 21:18:14 +0530

    feat: add additional product routes for new, hot, and package products

--- Furniture_Backend/Routes/productRoutes.js
+++ Furniture_Backend/Routes/productRoutes.js
@@ -4,6 +4,9 @@ const productController = require("../controllers/productController");
 
 router.get("/", productController.getAllProducts);
 router.post("/", productController.addProduct);
+router.get("/new", productController.getNewProducts);
+router.get("/hot", productController.getHotProducts);
+router.get("/package", productController.getPackageProducts);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 
