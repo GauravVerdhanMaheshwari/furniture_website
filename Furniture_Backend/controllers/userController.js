c3c6459d3b350d876b0ba8fed962e87b857b7951
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-30 19:07:16 +0530

    feat: implement user controller with CRUD operations for user management

---
+++ Furniture_Backend/controllers/userController.js
@@ -0,0 +1,42 @@
+const user = require("../Models/user");
+
+exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await user.find();
+    console.log("Fetched users:", users);
+    res.json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Add a new user
+exports.addUser = async (req, res, next) => {
+  try {
+    const newUser = new user(req.body);
+    await newUser.save();
+    res.status(201).json({ message: "User added successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update a user
+exports.updateUser = async (req, res, next) => {
+  try {
+    await user.findByIdAndUpdate(req.params.id, req.body);
+    res.json({ message: "User updated successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete a user
+exports.deleteUser = async (req, res, next) => {
+  try {
+    await user.findByIdAndDelete(req.params.id);
+    res.json({ message: "User deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
