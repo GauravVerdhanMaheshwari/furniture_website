1f0ad4739b0c0b69cc4fad9d7e1bb513c803e2ee
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-30 21:02:17 +0530

    feat: implement order controller with CRUD operations for orders

---
+++ Furniture_Backend/controllers/orderController.js
@@ -0,0 +1,37 @@
+const Order = require("../Models/order");
+
+exports.getAllOrders = async (req, res, next) => {
+  try {
+    const orders = await Order.find()
+      .populate("user")
+      .populate("products.product");
+    res.json(orders);
+  } catch (error) {
+    next(error);
+  }
+};
+exports.addOrder = async (req, res, next) => {
+  try {
+    const newOrder = new Order(req.body);
+    await newOrder.save();
+    res.status(201).json({ message: "Order placed successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.updateOrder = async (req, res, next) => {
+  try {
+    await Order.findByIdAndUpdate(req.params.id, req.body);
+    res.json({ message: "Order updated successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+exports.deleteOrder = async (req, res, next) => {
+  try {
+    await Order.findByIdAndDelete(req.params.id);
+    res.json({ message: "Order deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
