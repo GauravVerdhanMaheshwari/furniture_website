61cdeec93c7ca7fdebbb1adb20585ceaaa605fcc
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-05-30 21:02:21 +0530

    feat: add user routes for CRUD operations

---
+++ Furniture_Backend/Routes/orderRouters.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/userController");
+
+// Get all users
+router.get("/", userController.getAllUsers);
+
+// Add a new user
+router.post("/", userController.addUser);
+
+// Update a user
+router.put("/:id", userController.updateUser);
+
+// Delete a user
+router.delete("/:id", userController.deleteUser);
