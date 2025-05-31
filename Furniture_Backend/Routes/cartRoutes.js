917fd07245dcb70d52b00d91e065c9aeea6db407
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-01 00:35:16 +0530

    fix: update POST /add route to use getCartForUser controller

--- Furniture_Backend/Routes/cartRoutes.js
+++ Furniture_Backend/Routes/cartRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const cartController = require("../controllers/cartControllers");
 
 // Cart routes
-router.post("/add", cartController.addItemToCart);
+router.post("/add", cartController.getCartForUser);
 router.get("/:userId", cartController.getCartForUser);
 router.delete("/remove", cartController.removeItemFromCart);
 router.delete("/clear", cartController.clearCartForUser);
