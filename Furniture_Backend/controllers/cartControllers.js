b3e9b6f0d4ba90ef367418e17cb7365401d16f0b
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-01 00:35:21 +0530

    refactor: remove addItemToCart function and update getCartForUser to return empty cart if not found

--- Furniture_Backend/controllers/cartControllers.js
+++ Furniture_Backend/controllers/cartControllers.js
@@ -1,46 +1,5 @@
 const Cart = require("../Models/cart");
 
-// Add item to cart
-exports.addItemToCart = async (req, res) => {
-  const { userId, items } = req.body;
-  const productId = items[0].productId; //was expecting this as an single item, but it can be an array of items
-  const quantity = items[1] || 1; //was expecting this as an single item, but it can be an array of items
-
-  if (!userId) {
-    return res.status(400).json({ message: "Missing required userID fields" });
-  } else if (!productId) {
-    return res
-      .status(400)
-      .json({ message: "Missing required productID fields" });
-  } else if (!quantity) {
-    return res.status(400).json({ message: "Missing quantity" });
-  }
-
-  try {
-    let cart = await Cart.findOne({ userId });
-
-    if (!cart) {
-      cart = new Cart({ userId, items: [] });
-    }
-
-    const existingItem = cart.items.find(
-      (item) => item.productId.toString() === productId
-    );
-
-    if (existingItem) {
-      existingItem.quantity += quantity;
-    } else {
-      cart.items.push({ productId, quantity });
-    }
-
-    await cart.save();
-    res.status(200).json(cart);
-  } catch (error) {
-    console.error("Error adding item to cart:", error);
-    res.status(500).json({ message: "Error adding item to cart", error });
-  }
-};
-
 // Get cart for user
 exports.getCartForUser = async (req, res) => {
   const { userId } = req.params;
@@ -49,12 +8,11 @@ exports.getCartForUser = async (req, res) => {
     const cart = await Cart.findOne({ userId }).populate("items.productId");
 
     if (!cart) {
-      return res.status(404).json({ message: "Cart not found" });
+      return res.status(200).json({ userId, items: [] }); // Returning empty cart if not found
     }
 
     res.status(200).json(cart);
   } catch (error) {
-    console.error("Error fetching cart:", error);
     res.status(500).json({ message: "Error fetching cart", error });
   }
 };
