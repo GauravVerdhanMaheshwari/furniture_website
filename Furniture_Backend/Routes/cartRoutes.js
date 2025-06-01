20eec1c30587ed2c9d73634543d2440319c55dc4
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-02 00:40:48 +0530

    fix: correct POST /add route to use addToCart controller

--- Furniture_Backend/Routes/cartRoutes.js
+++ Furniture_Backend/Routes/cartRoutes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const cartController = require("../controllers/cartControllers");
 
 // Cart routes
-router.post("/add", cartController.getCartForUser);
+router.post("/add", cartController.addToCart);
 router.get("/:userId", cartController.getCartForUser);
+router.put("/update", cartController.updateCartForUser);
 router.delete("/remove", cartController.removeItemFromCart);
 router.delete("/clear", cartController.clearCartForUser);
 
