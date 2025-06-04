f1c836dee4ab7c2b48f449aa149ee200c84c9383
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-04 23:25:47 +0530

    feat: add validation for user creation and enhance user deletion process with cart and orders cleanup

--- Furniture_Backend/controllers/userController.js
+++ Furniture_Backend/controllers/userController.js
@@ -26,6 +26,20 @@ exports.getUserById = async (req, res, next) => {
 // Add a new user
 exports.addUser = async (req, res, next) => {
   try {
+    if (
+      !req.body.name ||
+      !req.body.email ||
+      !req.body.password ||
+      !req.body.address ||
+      !req.body.phone
+    ) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // Check if user already exists
+    const existingUser = await user.findOne({ email: req.body.email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
     const newUser = new user(req.body);
     await newUser.save();
     res.status(201).json({ message: "User added successfully" });
@@ -57,6 +71,47 @@ exports.deleteUser = async (req, res, next) => {
     if (!deletedUser) {
       return res.status(404).json({ message: "User not found" });
     }
+
+    // Delete cart associated with the user
+
+    {
+      fetch(`http://localhost:3000/api/cart/delete/${req.params.id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to delete cart for user");
+          }
+          return response.json();
+        })
+        .catch((error) => {
+          console.error("Error deleting cart:", error);
+        });
+    }
+
+    // Delete orders associated with the user
+
+    {
+      fetch(`http://localhost:3000/api/orders/delete/${req.params.id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Failed to delete orders for user");
+          }
+          return response.json();
+        })
+        .catch((error) => {
+          console.error("Error deleting orders:", error);
+        });
+    }
+
     res.json({ message: "User deleted successfully" });
   } catch (error) {
     next(error);
