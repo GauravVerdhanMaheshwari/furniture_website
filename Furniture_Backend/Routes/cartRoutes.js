4f0c95d3b9413df6a76366dc4d4b6b095b5b2c81
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-04 23:25:19 +0530

    feat: fix import path for cartController and add cart management functions

--- Furniture_Backend/Routes/cartRoutes.js
+++ Furniture_Backend/Routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const cartController = require("../controllers/cartControllers");
+const cartController = require("../controllers/cartController");
 
 // Cart routes
 router.post("/add", cartController.addToCart);
