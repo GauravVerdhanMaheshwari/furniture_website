165cdb655a7f3ff71fb9ed9fc35097fcf1ff3574
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-05 19:54:33 +0530

    refactor: rename historyRoutes.js to correct spelling and update import syntax

--- Furniture_Backend/Routes/histroyRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-import HistoryController from "../controllers/historyController.js";
-
-router.get("/", HistoryController.getAllHistory);
-router.post("/", HistoryController.addHistory);
-
-export default router;
