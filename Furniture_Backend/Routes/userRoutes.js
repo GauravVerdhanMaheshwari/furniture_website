54cf67fee0d9be0ac741d5acf98fbb9f9fbab7d3
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-06 01:41:30 +0530

    feat: add routes for current user retrieval and user login

--- Furniture_Backend/Routes/userRoutes.js
+++ Furniture_Backend/Routes/userRoutes.js
@@ -5,8 +5,14 @@ const userController = require("../controllers/userController");
 // Get all users
 router.get("/", userController.getAllUsers);
 
+// Get the current user
+router.get("/me", userController.getUserByEmail);
+
 // Get a user by ID
 router.get("/:id", userController.getUserById);
+
+// Login a user
+router.post("/login", userController.loginUser);
 
 // Add a new user
 router.post("/", userController.addUser);
