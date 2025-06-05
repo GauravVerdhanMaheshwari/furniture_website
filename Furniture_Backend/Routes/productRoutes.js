684f8bf651ad65f6f894ce67b9d9ff00c227a552
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-05 21:29:20 +0530

    feat: add route for retrieving product by ID

--- Furniture_Backend/Routes/productRoutes.js
+++ Furniture_Backend/Routes/productRoutes.js
@@ -7,6 +7,7 @@ router.post("/", productController.addProduct);
 router.get("/new", productController.getNewProducts);
 router.get("/hot", productController.getHotProducts);
 router.get("/package", productController.getPackageProducts);
+router.get("/:id", productController.getProductById);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
 
