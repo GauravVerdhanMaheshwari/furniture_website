4310c3b773554afd3770bfff250f06e79f8b0046
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-05 19:54:50 +0530

    refactor: correct route import paths and clean up commented code in server.js

--- Furniture_Backend/server.js
+++ Furniture_Backend/server.js
@@ -2,31 +2,29 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
-const productRoutes = require("./Routes/productRoutes");
-// const authRoutes = require("./Routes/authRoutes");
-const cartRoutes = require("./Routes/cartRoutes");
-// const orderRoutes = require("./Routes/orderRoutes");\
-const userRoutes = require("./Routes/userRoutes");
-const purchaseRoutes = require("./Routes/purchaseRoutes");
+const productRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const userRoutes = require("./routes/userRoutes");
+const purchaseRoutes = require("./routes/purchaseRoutes");
+const historyRoutes = require("./Routes/historyRoutes");
+
 const errorHandler = require("./middleware/errorHandler");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
-// API Routes
+// Routes
 app.use("/api/products", productRoutes);
-// app.use("/api/auth", authRoutes);
 app.use("/api/purchases", purchaseRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/users", userRoutes);
-// app.use("/api/orders", orderRoutes);
+app.use("/api/history", historyRoutes);
 
-// Error Handling Middleware
+// Error handling middleware
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
