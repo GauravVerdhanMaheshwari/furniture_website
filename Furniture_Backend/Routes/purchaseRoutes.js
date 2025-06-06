585bd053df7a0e329870c8e59f0e6bcfa1801189
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-06 14:16:33 +0530

    fix: remove commented-out updatePurchase route in purchaseRoutes

--- Furniture_Backend/Routes/purchaseRoutes.js
+++ Furniture_Backend/Routes/purchaseRoutes.js
@@ -5,5 +5,7 @@ const purchaseController = require("../controllers/purchaseControllers");
 router.get("/", purchaseController.getAllPurchases);
 router.get("/:id", purchaseController.getPurchaseById);
 router.post("/", purchaseController.addPurchase);
+// router.put("/:id", purchaseController.updatePurchase);
+router.delete("/:id", purchaseController.deletePurchase);
 
 module.exports = router;
