7867c07ac246a1bd9a610b1e018511fc5621707b
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-08 00:23:25 +0530

    feat: add ownerRoutes for managing purchases and products

---
+++ Furniture_Backend/Routes/ownerRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const ownerController = require("../controllers/ownerController");
+
+router.get("/purchases", ownerController.getAllPurchases);
+router.put("/purchases/accept/:id", ownerController.acceptPurchase);
+router.put("/purchases/reject/:id", ownerController.rejectPurchase);
+router.put("/purchases/deliver/:id", ownerController.deliverPurchase);
+router.put("/purchases/cancel/:id", ownerController.cancelPurchase);
+
+router.get("/product", ownerController.getAllProducts);
+router.get("/product/:id", ownerController.getProductById);
+router.post("/product/add", ownerController.addProduct);
+router.put("/product/:id", ownerController.updateProduct);
+router.delete("/product/:id", ownerController.deleteProduct);
+
+module.exports = router;
