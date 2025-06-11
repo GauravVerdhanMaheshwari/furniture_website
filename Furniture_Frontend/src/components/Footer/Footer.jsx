9d81c15d2fa85bba10c67ca2bf9e45d95255c60e
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-11 13:51:35 +0530

    refactor: update Footer component styling for improved aesthetics and user experience

--- Furniture_Frontend/src/components/Footer/Footer.jsx
+++ Furniture_Frontend/src/components/Footer/Footer.jsx
@@ -3,19 +3,25 @@ import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-white p-3 md:p-5 border-y w-full">
-      <div className="container mx-auto py-2 md:py-3 flex flex-col md:flex-row items-center justify-between gap-4">
-        <span className="text-sm md:text-base text-gray-500 text-center md:text-left">
-          © 2025 Geetanjali Furniture . All rights reserved.
+    <footer className="bg-[#3F4238] border-t-2 border-[#6B705C] p-4 md:p-6 w-full">
+      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-4">
+        <span className="text-sm md:text-base text-[#FFE8D6] text-center md:text-left">
+          © 2025 <span className="font-semibold">Geetanjali Furniture</span>.
+          All rights reserved.
         </span>
 
         <div className="flex space-x-4 md:space-x-5">
           <Link
             to="https://g.co/kgs/HL9xtGk"
             target="_blank"
-            className="text-gray-500 hover:text-gray-700 transition-colors"
+            className="hover:scale-105 transition-transform duration-200"
+            aria-label="Location on Google Maps"
           >
-            <img src="map.webp" alt="map" className="w-8 h-8 md:w-10 md:h-10" />
+            <img
+              src="map.webp"
+              alt="Geetanjali Furniture Location"
+              className="w-8 h-8 md:w-10 md:h-10 rounded-full border-2 border-[#CB997E] hover:border-[#DDBEA9]"
+            />
           </Link>
         </div>
       </div>
