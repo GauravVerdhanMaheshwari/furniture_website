0d2c48eb17cd8824416e52caae73b68b89d9cc95
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-11 13:52:04 +0530

    refactor: enhance Hero component styling and layout for improved user experience

--- Furniture_Frontend/src/components/Home/Hero/Hero.jsx
+++ Furniture_Frontend/src/components/Home/Hero/Hero.jsx
@@ -3,22 +3,24 @@ import { useNavigate } from "react-router-dom";
 
 function Hero() {
   const navigate = useNavigate();
+
   return (
-    <div className="bg-gray-100 flex flex-col md:flex-row min-h-[60vh] mt-25">
-      <div className="bg-gray-200 w-full md:w-1/2 flex flex-col justify-center items-center md:items-end p-8 md:p-20">
-        <h1 className="text-2xl md:text-4xl text-center md:text-right">
-          Welcome to our shop
+    <div className="bg-[#FFE8D6] flex flex-col md:flex-row min-h-[60vh] mt-20">
+      <div className="bg-[#DDBEA9] w-full md:w-1/2 flex flex-col justify-center items-center md:items-end p-8 md:p-20">
+        <h1 className="text-3xl md:text-5xl font-semibold text-[#3F4238] text-center md:text-right">
+          Welcome to Our Shop
         </h1>
-        <p className="text-base md:text-lg mt-4 text-center md:text-right">
-          Find the best furniture for you
+        <p className="text-lg md:text-xl mt-4 text-[#6B705C] text-center md:text-right">
+          Find timeless furniture tailored for you
         </p>
         <button
-          className="mt-6 px-4 md:px-5 py-2 md:py-2.5 rounded-xl bg-red-200 cursor-pointer hover:bg-red-300 hover:scale-105 transition-all duration-300 text-black font-bold text-lg md:text-xl active:scale-95 active:bg-red-400"
+          className="mt-6 px-6 py-3 rounded-xl bg-[#CB997E] text-[#3F4238] font-bold text-lg md:text-xl hover:bg-[#B98B73] hover:scale-105 active:scale-95 transition-all duration-300 cursor-pointer"
           onClick={() => navigate("/products")}
         >
-          Shop Now !
+          Shop Now
         </button>
       </div>
+
       <div className="w-full md:w-1/2 flex justify-center items-center min-h-[300px] md:min-h-0">
         <img
           src="hero_image.jpg"
