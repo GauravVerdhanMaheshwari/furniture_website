f169d2ae2ad0d19627bd01bc66f228aa2aff4582
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-12 09:13:40 +0530

    feat: create indexComponents file to organize component exports

---
+++ Furniture_Frontend/src/components/indexComponents.js
@@ -0,0 +1,35 @@
+import AdminFooter from "./AdminFooter/AdminFooter";
+import AdminHeader from "./AdminHeader/AdminHeader";
+import CartComponent from "./CartComponent/CartComponent";
+import Footer from "./Footer/Footer";
+import FurnitureCard from "./FurnitureCard/FurnitureCard";
+import Header from "./Header/Header";
+import Hero from "./Hero/Hero";
+import HistoryBuys from "./HistoryBuys/HistoryBuys";
+import Order from "./Order/Order";
+import Product from "./Product/Product";
+import Purchase from "./Purchase/Purchase";
+import Search from "./Search/Search";
+import SearchFilter from "./SearchFilter/SearchFilter";
+import Suggestions from "./Suggestions/Suggestions";
+import UserForm from "./UserForm/UserForm";
+import FilterPanel from "./FilterPanel/FilterPanel";
+
+export {
+  AdminFooter,
+  AdminHeader,
+  CartComponent,
+  Footer,
+  FurnitureCard,
+  Header,
+  Hero,
+  HistoryBuys,
+  Order,
+  Product,
+  Purchase,
+  Search,
+  SearchFilter,
+  Suggestions,
+  UserForm,
+  FilterPanel,
+};
