3893b3090e14d422eacfca6190914c821b1f94b0
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-12 09:13:47 +0530

    feat: add indexPage component to consolidate page exports

--- Furniture_Frontend/src/components/Home/index.js
+++
@@ -1,4 +0,0 @@
-import Hero from "./Hero/Hero";
-import Suggestions from "./Suggestions/Suggestions";
-
-export { Hero, Suggestions };
---
+++ Furniture_Frontend/src/pages/indexPage.js
@@ -0,0 +1,39 @@
+import Page404 from "./404/Page404";
+import AboutUs from "./AboutUs/AboutUs";
+import AdminAddProduct from "./AdminAddProduct/AdminAddProduct";
+import AdminEditProduct from "./AdminEditProduct/AdminEditProduct";
+import AdminHome from "./AdminHome/AdminHome";
+import AdminLogin from "./AdminLogin/AdminLogin";
+import AdminOrder from "./AdminOrder/AdminOrder";
+import AdminProducts from "./AdminProducts/AdminProducts";
+import AdminProfile from "./AdminProfile/AdminProfile";
+import Cart from "./Cart/Cart";
+import CheckOut from "./Checkout/Checkout";
+import Contacts from "./Contacts/Contacts";
+import Furniture from "./Furniture/Furniture";
+import Home from "./Home/Home";
+import Login from "./Login/Login";
+import Products from "./Products/Products";
+import Profile from "./Profile/Profile";
+import Register from "./Register/Register";
+
+export {
+  Page404,
+  AboutUs,
+  AdminAddProduct,
+  AdminEditProduct,
+  AdminHome,
+  AdminLogin,
+  AdminOrder,
+  AdminProducts,
+  AdminProfile,
+  Cart,
+  CheckOut,
+  Contacts,
+  Furniture,
+  Home,
+  Login,
+  Products,
+  Profile,
+  Register,
+};
