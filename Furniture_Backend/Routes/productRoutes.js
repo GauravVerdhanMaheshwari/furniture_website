1ab2f69a3390266da704090590b375715cf36cb0
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-13 15:33:28 +0530

    refactor: reorder product routes for improved clarity and organization

--- Furniture_Backend/Routes/productRoutes.js
+++ Furniture_Backend/Routes/productRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get("/", productController.getAllProducts);
-// router.post("/", productController.addProduct);
 router.get("/new", productController.getNewProducts);
 router.get("/hot", productController.getHotProducts);
 router.get("/package", productController.getPackageProducts);
+
+router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 router.put("/:id", productController.updateProduct);
 router.delete("/:id", productController.deleteProduct);
