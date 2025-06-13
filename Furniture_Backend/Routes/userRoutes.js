6e658a98dfcfb9fde53b262f4ced03c1ed6a0f7a
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-13 15:34:05 +0530

    refactor: standardize comment formatting for user routes

--- Furniture_Backend/Routes/userRoutes.js
+++ Furniture_Backend/Routes/userRoutes.js
@@ -2,25 +2,16 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
 
-// Get all users
+// GET all users
 router.get("/", userController.getAllUsers);
 
-// Get the current user
-router.get("/me", userController.getUserByEmail);
-
-// Get a user by ID
+// GET a single user by ID
 router.get("/:id", userController.getUserById);
 
-// Login a user
-router.post("/login", userController.loginUser);
-
-// Add a new user
-router.post("/", userController.addUser);
-
-// Update a user
+// PUT update a user
 router.put("/:id", userController.updateUser);
 
-// Delete a user
+// DELETE a user
 router.delete("/:id", userController.deleteUser);
 
 module.exports = router;
