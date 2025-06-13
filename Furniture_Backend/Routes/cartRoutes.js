fc6e52485f09c83c4b421a70a8d6fabb80f32684
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-13 15:32:44 +0530

    fix: reorder cart route for user retrieval to maintain logical flow

--- Furniture_Backend/Routes/cartRoutes.js
+++ Furniture_Backend/Routes/cartRoutes.js
@@ -4,9 +4,9 @@ const cartController = require("../controllers/cartController");
 
 // Cart routes
 router.post("/add", cartController.addToCart);
-router.get("/:userId", cartController.getCartForUser);
 router.put("/update", cartController.updateCartForUser);
 router.delete("/remove", cartController.removeItemFromCart);
 router.delete("/clear", cartController.clearCartForUser);
+router.get("/:userId", cartController.getCartForUser);
 
 module.exports = router;
