8ddfd50757f715b610c86e5695dca6554cf0315c
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-13 15:33:47 +0530

    refactor: reorder purchase route definitions for consistency

--- Furniture_Backend/Routes/purchaseRoutes.js
+++ Furniture_Backend/Routes/purchaseRoutes.js
@@ -3,9 +3,8 @@ const router = express.Router();
 const purchaseController = require("../controllers/purchaseControllers");
 
 router.get("/", purchaseController.getAllPurchases);
-router.get("/:id", purchaseController.getPurchaseById);
 router.post("/", purchaseController.addPurchase);
-// router.put("/:id", purchaseController.updatePurchase);
+router.get("/:id", purchaseController.getPurchaseById);
 router.delete("/:id", purchaseController.deletePurchase);
 
 module.exports = router;
