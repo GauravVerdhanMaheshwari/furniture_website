4a15024253019cbdcb6bba2d5a05199c81dd5faa
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-14 16:46:34 +0530

    refactor: correct case of controller import in ownerRoutes

--- Furniture_Backend/Routes/ownerRoutes.js
+++ Furniture_Backend/Routes/ownerRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const ownerController = require("../Controllers/ownerController");
+const ownerController = require("../controllers/ownerController");
 
 // Product Management
 router.get("/products", ownerController.getAllProducts);
@@ -13,6 +13,7 @@ router.delete("/product/:id", ownerController.deleteProduct);
 router.patch("/product/reject/:id", ownerController.rejectProduct);
 
 // Purchase Management
+router.get("/purchases", ownerController.getAllPurchases);
 router.patch("/purchase/accept/:id", ownerController.acceptPurchase);
 router.patch("/purchase/reject/:id", ownerController.rejectPurchase);
 router.patch("/purchase/deliver/:id", ownerController.deliverPurchase);
