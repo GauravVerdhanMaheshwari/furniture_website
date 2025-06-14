4da372eea2459506448d516e3cc92806b54d25f5
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-14 16:46:08 +0530

    refactor: add admin login route to authRoutes

--- Furniture_Backend/Routes/authRoutes.js
+++ Furniture_Backend/Routes/authRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const ownerController = require("../controllers/ownerController");
 
 router.post("/login", userController.loginUser);
 router.post("/register", userController.addUser);
 router.post("/me", userController.getUserByEmail);
+router.post("/admin/login", ownerController.loginOwner);
 
 module.exports = router;
