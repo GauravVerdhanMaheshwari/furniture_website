caff9aafcb5816047ef57b8ea6a5b443b2a4b67a
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-24 02:24:15 +0530

    fix: add Package component to indexComponents export

--- Furniture_Frontend/src/components/indexComponents.js
+++ Furniture_Frontend/src/components/indexComponents.js
@@ -5,6 +5,7 @@ import FurnitureCard from "./FurnitureCard/FurnitureCard";
 import Header from "./Header/Header";
 import Hero from "./Hero/Hero";
 import Product from "./Product/Product";
+import Package from "./Package/Package";
 import Search from "./Search/Search";
 import SearchFilter from "./SearchFilter/SearchFilter";
 import Suggestions from "./Suggestions/Suggestions";
@@ -19,6 +20,7 @@ export {
   Header,
   Hero,
   Product,
+  Package,
   Search,
   SearchFilter,
   Suggestions,
