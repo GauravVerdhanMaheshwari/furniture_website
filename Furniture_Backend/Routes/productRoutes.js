52f0de6bcf7f542139d1b87e842534abafc0558d
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-24 02:25:20 +0530

    feat: remove package route from product routes

--- Furniture_Backend/Routes/productRoutes.js
+++ Furniture_Backend/Routes/productRoutes.js
@@ -7,7 +7,6 @@ router.get("/categories", productController.getProductCategories);
 
 router.get("/new", productController.getNewProducts);
 router.get("/hot", productController.getHotProducts);
-router.get("/package", productController.getPackageProducts);
 router.get("/", productController.getAllProducts);
 router.get("/:id", productController.getProductById);
 router.put("/:id", productController.updateProduct);
