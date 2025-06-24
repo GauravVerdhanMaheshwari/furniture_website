ee533e11d2602a0d19103418709d5040659f9ff1
GauravVerdhanMaheshwari <gauravverdhan1702@gmail.com>
2025-06-24 15:56:42 +0530

    fix: add Link import for navigation in AdminPackages component

--- Furniture_Frontend/src/pages/AdminPackages/AdminPackages.jsx
+++ Furniture_Frontend/src/pages/AdminPackages/AdminPackages.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { Search, Package } from "../../components/indexComponents";
 
 function AdminPackages() {
